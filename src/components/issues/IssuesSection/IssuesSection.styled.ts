c5aba61e369428d6230fd27f8b1612567d8074ce
HaJunRyu <hjr4557@naver.com>
2023-02-24 23:39:58 +0900

    Feat: bookmark된 레포가 없을떄 Issue Page에 보여줄 UI를 구현한다

--- src/components/issues/IssuesSection/IssuesSection.styled.ts
+++ src/components/issues/IssuesSection/IssuesSection.styled.ts
@@ -1,7 +1,34 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+export const Heading = styled.h2`
+  margin-bottom: 1.6rem;
+`;
 
 export const Issues = styled.ul`
   display: flex;
   flex-flow: column nowrap;
   gap: 1rem;
+`;
+
+export const BookmarkIsEmptyContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  padding: 3rem 0;
+`;
+
+export const EmptyMessage = styled.em`
+  font-style: normal;
+  font-size: 3rem;
+  font-weight: 700;
+`;
+
+export const GoToSearchPage = styled(Link)`
+  margin-top: 3rem;
+  padding: 1.2rem;
+  font-size: 2rem;
+  background: #0969da;
+  color: #ffffff;
+  border-radius: 0.6rem;
 `;
--- src/components/issues/IssuesSection/IssuesSection.tsx
+++ src/components/issues/IssuesSection/IssuesSection.tsx
@@ -1,4 +1,6 @@
 import { Issue } from '@/components/issues';
+import { ROUTE } from '@/constants/route';
+import useBookmarkValue from '@/hooks/useBookmarkValue';
 import { IssueType } from '../Issue/Issue';
 import * as Styled from './IssuesSection.styled';
 
@@ -7,12 +9,25 @@ interface IssuesSectionProps {
 }
 
 const IssuesSection = ({ issues }: IssuesSectionProps) => {
+  const bookmark = useBookmarkValue();
   return (
-    <Styled.Issues>
-      {issues.map(issue => (
-        <Issue issue={issue} key={issue.html_url} />
-      ))}
-    </Styled.Issues>
+    <section>
+      <Styled.Heading>Bookmark Repository Issues</Styled.Heading>
+      <Styled.Issues>
+        {bookmark.length > 0 ? (
+          <>
+            {issues.map(issue => (
+              <Issue issue={issue} key={issue.html_url} />
+            ))}
+          </>
+        ) : (
+          <Styled.BookmarkIsEmptyContainer>
+            <Styled.EmptyMessage>북마크 된 저장소가 없습니다</Styled.EmptyMessage>
+            <Styled.GoToSearchPage to={ROUTE.SEARCH}>Repository 검색 🔍</Styled.GoToSearchPage>
+          </Styled.BookmarkIsEmptyContainer>
+        )}
+      </Styled.Issues>
+    </section>
   );
 };
 
