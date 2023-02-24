30ee3c09720506fa472b3f3f9e0512d05db75136
HaJunRyu <hjr4557@naver.com>
2023-02-24 22:18:45 +0900

    Refactor: issues 라우트의 필요없는 params를 제거한다

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: `${ROUTE.ISSUES}/:owner/:repo`,
+        path: ROUTE.ISSUES,
         element: <Issues />,
       },
       {
