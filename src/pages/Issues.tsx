4838102b3b01b7e6f9e295dd35fbe6b4457f516e
HaJunRyu <hjr4557@naver.com>
2023-02-24 22:27:20 +0900

    Feat: bookmark된 저장소의 이슈를 Promise.all을 이용해 모두 불러온다

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -1,4 +1,38 @@
+import issuesService from '@/api/service/issues';
+import useBookmarkValue from '@/hooks/useBookmarkValue';
+import { IssueItem } from '@/types/dto/issues';
+import { useEffect, useState } from 'react';
+
 const Issues = () => {
+  const [bookmarkRepoIssues, setBookmarkRepoIssues] = useState<IssueItem[]>([]);
+  const bookmark = useBookmarkValue();
+
+  useEffect(() => {
+    const fetchIssues = async () => {
+      const fetchBookmarkIssues = bookmark.map(async ({ owner, name }) => {
+        const response = await issuesService.getRepositoryIssues({
+          owner: owner.login,
+          repo: name,
+          page: 1,
+        });
+
+        return response.data;
+      });
+
+      const bookmarkIssuesResponse = await Promise.all(fetchBookmarkIssues);
+      const bookmarkIssues = bookmarkIssuesResponse.reduce((acc, issues) => {
+        return [...acc, ...issues].sort((issueA, issueB) => {
+          const issueADate = new Date(issueA.created_at).valueOf();
+          const issueBDate = new Date(issueB.created_at).valueOf();
+          return issueBDate - issueADate;
+        });
+      }, []);
+
+      setBookmarkRepoIssues(bookmarkIssues);
+    };
+
+    fetchIssues();
+  }, []);
   return <div>Issues</div>;
 };
 
